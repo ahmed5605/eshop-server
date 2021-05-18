9491a72c5313b0fd75e4263dda26fd0dccee197e
Zaid Ahmed <ahmedzaidmohd.com>
2021-05-18 10:47:55 +0530

    first commit

--- app.js
+++ app.js
@@ -54,10 +54,10 @@ mongoose.connect(process.env.CONNECTION_STRING,{
 //     console.log("server is running")
 // })
 
-// var server = app.listen(process.env.PORT || 3000, function () {
-//    var port = server.address().port; 
-//    console.log("Express is working on port " + port); 
-//   });
+var server = app.listen(process.env.PORT || 3000, function () {
+   var port = server.address().port; 
+   console.log("Express is working on port " + port); 
+  });
 
 
 
