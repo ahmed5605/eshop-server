9d1b7d705b2c9521b64f86934480374c342945af
Zaid Ahmed <ahmedzaidmohd.com>
2021-05-18 22:54:56 +0530

    first commit

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,9 +11,11 @@ function authJwt(){
     })
     .unless({
          path: [
+            { url: /\/api\/v1\/users\/.*/,methods: ['GET']},
             {url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
             `${api}/users/register`
         ]
